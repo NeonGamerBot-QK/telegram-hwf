6a7ed77323a551e148b0535d63c6a74fcc8b47e7
Saahil <neon@saahild.com>
2025-05-21 23:06:59 -0400

    update: finish up msg handler

--- src/commands/friend-code.ts
+++ src/commands/friend-code.ts
@@ -27,8 +27,60 @@ export default {
     if (!friendCode) {
       return bot.sendMessage(
         msg.chat.id,
-        `Here is your friend code!: ${userData.id}`,
+        `Here is your friend code!: ${userData.id}, Your name will be set to \`${msg.chat.id}\` so make sure to tell whoever ur sending this to to set ur name under that chat id`,
       );
     }
+    // try to get such friend
+    //@ts-ignore stfu
+    if(isNaN(friendCode.trim())) {
+      return bot.sendMessage(
+        msg.chat.id,
+        `Invalid friend code, please try again`,
+      );
+    }
+    const friend = await prisma.user.findFirst({
+      where: {
+        id: parseInt(friendCode.trim()),
+      },
+    });
+    if(!friend) {
+      return bot.sendMessage(
+        msg.chat.id,
+        `No such user with id ${friendCode.trim()}`,
+      );
+    }
+    const currentUserData = await prisma.user.findFirst({
+      where: {
+        telegramId: encryptString(msg.from!.id.toString()),
+      },
+    })
+    const msgg = await bot.sendMessage(msg.chat.id, "Adding you as a friend, please wait...")
+    const myfriendsList = JSON.parse(currentUserData?.friendsList || "[]")
+    myfriendsList.push(decryptString(friend.telegramId))
+    const friendsFriendList = JSON.parse(friend.friendsList || "[]")
+    friendsFriendList.push(decryptString(currentUserData?.telegramId || ""))
+    // update the user
+     await Promise.all([prisma.user.update({
+      //@ts-ignore
+      where: {
+        telegramId: encryptString(msg.from!.id.toString())!,
+      },
+      data: {
+        friendsList: decryptString(JSON.stringify(myfriendsList))
+      },
+    }),
+     prisma.user.update({
+      //@ts-ignore
+      where: {
+        telegramId: encryptString(friend.telegramId)!,
+      },
+      data: {
+        friendsList: decryptString(JSON.stringify(friendsFriendList))
+      },
+    })])
+await bot.editMessageText("Added you as a friend!", {
+  chat_id: msg.chat.id,
+  message_id: msgg.message_id,
+})
   },
 };
